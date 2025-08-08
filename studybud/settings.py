fd199a7894a517978c5d5d259fbfc30830d6dd7c
Satyaswarup11 <swarupsai112002@gmail.com>
2025-08-08 19:47:42 +0530

    Second commit

--- studybud/settings.py
+++ studybud/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -58,6 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'studybud.urls'
@@ -131,6 +133,17 @@ MEDIA_URL = 'images/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',  # Add your static files directory here
 ]
+
+
+
+# Folder where static files will be collected for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Whitenoise storage (for Render)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
 
 MEDIA_ROOT = BASE_DIR / 'static/images'  # Directory for media files
 
